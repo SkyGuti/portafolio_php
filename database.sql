247a96dc0fc612ae3109ce6fb96ec69fb71989a0
SkyGuti <elkin6668@gmail.com>
2026-05-30 20:26:06 -0500

    Actualizo archivo database.sql

--- database.sql
+++ database.sql
@@ -1,13 +1,10 @@
-CREATE DATABASE IF NOT EXISTS catalogo_productos_db;
-USE catalogo_productos_db;
-
-CREATE TABLE administradores (
+CREATE TABLE IF NOT EXISTS administradores (
     id INT AUTO_INCREMENT PRIMARY KEY,
     usuario VARCHAR(50) NOT NULL UNIQUE,
     clave VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE mensajes (
+CREATE TABLE IF NOT EXISTS mensajes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     correo VARCHAR(100) NOT NULL,
